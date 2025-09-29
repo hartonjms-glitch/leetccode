15897abab4b003fbda8bf86e9d2d6fddd922814f
hartonjms-glitch <hartonjms@gmail.com>
2025-09-29 09:21:37 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (38.57%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int total=0;
+        int sum=n*(n+1)/2;
+        for(int i=0;i<n;i++){
+             total +=nums[i];
+        }
+        return sum-total;
+        
+    }
+};
